4ba567267bfc2b410e873a1b2c47789f6ec2a252
CebolaBros64 <15020325+CebolaBros64@users.noreply.github.com>
2021-04-01 17:25:56 -0300

    clean up

--- settings.py
+++
@@ -1,4 +0,0 @@
-recentROMs = [
-            {'rom': "Mr. Driller G.cue", 'path': "F:\\! ROMs\\Sony Playstation\\Mr. Driller G (JPN)\\"},
-            {'rom': "Dr. Mario & Puzzle League.gba", 'path': "F:\\! ROMs\\Nintendo Game Boy Advance\\"}
-            ]
